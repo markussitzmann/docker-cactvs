db38c7600b513f10e444fca1f78629ec0e6c6ca8
Markus Sitzmann <markus.sitzmann@gmail.com>
2021-09-13 23:52:42 +0200

    run time improvements

---
+++ context/app/pycactvs/docker-entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+if ! id "app" >/dev/null 2>&1; then
+    groupadd -g $CACTVS_GID app && \
+    useradd --shell /bin/bash -u $CACTVS_UID -g $CACTVS_GID -o -c "" -M app
+fi
+
+gosu app "$@"
+
---
+++ context/app/pycactvs/script.py
@@ -0,0 +1,5 @@
+from pycactvs import Ens
+
+e = Ens("CCO")
+
+print(e)
