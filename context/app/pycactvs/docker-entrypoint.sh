62ed260f256e5984943cbf6f209a4dfb4231fbb6
sitzmann <marrkus.sitzmann@gmail.com>
2023-07-29 22:50:02 +0200

    post release 1.1

--- context/app/pycactvs/docker-entrypoint.sh
+++ context/app/pycactvs/docker-entrypoint.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 set -e
 
+source .env
+
 if ! id "app" >/dev/null 2>&1; then
     groupadd -g $CACTVS_GID app && \
     useradd --shell /bin/bash -u $CACTVS_UID -g $CACTVS_GID -o -c "" -M app
 fi
 
-PGM="/opt/cactvs/lib/cactvs3.4.8.19/lib/pycactvs"
+PGM="/opt/cactvs/lib/${CACTVS_VERSION}/pycactvs"
 
 gosu app "$PGM" -obd "$@"
 
